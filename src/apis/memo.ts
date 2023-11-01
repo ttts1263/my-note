ba1d3e3c20ba7fdaf6e5d1c06c6706502e9cab07
ttts1263 <ttts1263@gmail.com>
2023-11-01 20:34:36 +0900

    feat: add memo functions, fix memo styles

--- src/apis/memo.ts
+++ src/apis/memo.ts
@@ -1,12 +1,25 @@
+const memos = [
+  {
+    id: 1,
+    text: '메모1',
+  },
+  {
+    id: 2,
+    text: '메모2',
+  },
+]
+
+// CRUD (Create, Read, Upadte ,Dlete)
 export function getMemos() {
-  return [
-    {
-      id: 1,
-      text: '메모1',
-    },
-    {
-      id: 2,
-      text: '메모2',
-    },
-  ]
+  return memos
+}
+
+// createMemo, deletMemo
+
+export function upadtesetMemo(memoId: number, memoText: string) {
+  const memo = memos.find((memo) => memo.id === memoId)
+  if (memo !== undefined) {
+    memo.text = memoText
+  }
+  return memos
 }
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -3,7 +3,10 @@ import { Header } from '../../components/Header'
 import styled from '@emotion/styled'
 
 export function HomePage() {
+  // 데이터를 가져옴 (초기 데이터)
   const memos = getMemos()
+
+  // 데이터를 수정
 
   return (
     <main>
@@ -15,8 +18,7 @@ export function HomePage() {
         {memos.map((memo) => {
           return (
             <StyledMemo>
-              <div>{memo.id}</div>
-              <div className="text">{memo.text}</div>
+              <textarea>{memo.text}</textarea>
             </StyledMemo>
           )
         })}
@@ -26,11 +28,19 @@ export function HomePage() {
 }
 
 const StyledMemo = styled.div`
-  background-color: yellow;
-  div {
-    color: blue;
-  }
-  .text {
-    font-size: 40px;
+  margin-bottom: 32px;
+  padding: 12px;
+  border-radius: 12px;
+  box-shadow: 5px 5px 10px 3px lightgray;
+  background-color: #fefe6d;
+  font-size: 100px;
+  textarea {
+    font-size: 20px;
+    width: 100%;
+    min-height: 240px;
+    background-color: unset;
+    border: 0;
+    outline: 0;
+    resize: none;
   }
 `
